54df9144dca113e13703a0c2d1a2f23d738c3279
vikram shaw <vikram.shaw.3382@gmail.com>
2025-03-15 15:23:04 +0530

    Refactored and added input.txt file

--- CleanCode.java
+++ CleanCode.java
@@ -3,23 +3,21 @@ import java.io.*;
 abstract class ReaderWriter {
     public BufferedReader in;
     public PrintStream out;
-    protected ReaderWriter(BufferedReader br, PrintStream ps) throws IOException {
-        in = br;
-        out = ps;
-    }
 }
 
 class OfflineReaderWriter extends ReaderWriter {
     OfflineReaderWriter(String inputFileName, String outputFileName) throws IOException {
         // Read from inputFileName and write to outputFileName
-        super(new BufferedReader(new FileReader(inputFileName)), new PrintStream(new File(outputFileName)));
+        in = new BufferedReader(new FileReader(inputFileName));
+        out = new PrintStream(new File(outputFileName));
     }
 }
 
 class OnlineReaderWriter extends ReaderWriter {
     OnlineReaderWriter() throws IOException {
         // Read from / write to console / terminal 
-        super(new BufferedReader(new InputStreamReader(System.in)), new PrintStream(System.out));
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = System.out;
     }
 }
 
@@ -58,7 +56,7 @@ class ReaderWriterFactory {
 }
 
 class CleanCode {
-    public static void main(String str[]) throws IOException{
+    public static void main(String args[]) throws IOException{
         ReaderWriter reader = ReaderWriterFactory.get();
         String name = reader.in.readLine();
         reader.out.println("You name is " + name);
